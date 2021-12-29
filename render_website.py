769488ecdad63563a8b94bf30e2d12f683cf38cb
alexei <artalexeimail@gmail.com>
2021-12-29 20:01:47 +0300

    fix errors

--- render_website.py
+++ render_website.py
@@ -5,6 +5,9 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
+LINES_ON_PAGE = 5
+COLUMNS_ON_PAGE = 2
+
 
 def rebuild():
     env = Environment(
@@ -13,19 +16,20 @@ def rebuild():
     )
     template = env.get_template('template.html')
     with open("books.json", "r", encoding="utf-8") as file:
-        books = json.loads(file.read())
+        books = json.load(file)
 
-    books = list(chunked(books, 2))
+    pairs_books = list(chunked(books, COLUMNS_ON_PAGE))
+    splited_books = list(chunked(pairs_books, LINES_ON_PAGE))
 
     os.makedirs('pages', exist_ok=True)
-    books = list(chunked(books, 5))
-    for page, page_books in enumerate(books):
+
+    for page, page_books in enumerate(splited_books, start=1):
         rendered_page = template.render(
             books=page_books,
-            total_pages=len(books),
-            current_page=page + 1
+            total_pages=len(splited_books),
+            current_page=page
         )
-        with open(f'pages/index{page + 1}.html', 'w', encoding="utf8") as file:
+        with open(f'pages/index{page}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
     print("Site rebuilt")
